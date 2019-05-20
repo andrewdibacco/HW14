40526df5fb3336063992d38b7eee82be338f540f
andrewdibacco <48300806+andrewdibacco@users.noreply.github.com>
2019-05-20 15:04:22 -0400

    Add files via upload

---
+++ Students.API/Controllers/StudentAPIController.cs
@@ -0,0 +1,22 @@
+﻿using CollegeDBEF.DAL;
+using System.Linq;
+using System.Web.Http;
+
+namespace Students.API.Controllers
+{
+    public class StudentAPIController : ApiController
+    {
+        public string Get(int id)
+        {
+            string fullName = "";
+
+            using (var context = new CollegeContext())
+            {
+                var student = context.Students.Single(a => a.ID == id);
+                fullName = student.FName + " " + student.LName;
+            }
+
+            return fullName;
+        }
+    }
+}
